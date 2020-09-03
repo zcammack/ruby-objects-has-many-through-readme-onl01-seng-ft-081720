04af1d968536bc74a99ddc00ceed72d470b2f3fd
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 01:56:04 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,6 +14,12 @@ class Waiter
     end
 
     def new_meal(customer, total, tip=0)
-    Meal.new(self, customer, total, tip)
+      Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+      Meal.all.select do |meal|
+        meal.waiter == self #checking for waiter now
+      end
     end
 end
