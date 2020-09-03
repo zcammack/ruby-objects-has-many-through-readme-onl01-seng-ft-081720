fc3482165d64b1ffc10f716d5b39fcee14cfe156
Zackary Cammack <zack.cammack@gmail.com>
2020-09-03 01:58:22 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,4 +22,12 @@ class Waiter
         meal.waiter == self #checking for waiter now
       end
     end
+
+    def best_tipper
+      best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+ 
+    best_tipped_meal.customer
+  end
 end
